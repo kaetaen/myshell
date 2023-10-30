675a3b5657981543333957e1cd7362caf378a5dc
kaetaen <rubens.santos@hurb.com>
2023-10-30 14:07:26 -0300

    fix: disable autofold and rainbow fold

--- src/vim/init.lua
+++ src/vim/init.lua
@@ -29,32 +29,7 @@ require("lazy").setup({
     'lukas-reineke/indent-blankline.nvim',
     main = "ibl",
     config = function()
-    
-      local highlight = {
-        "RainbowRed",
-        "RainbowYellow",
-        "RainbowBlue",
-        "RainbowOrange",
-        "RainbowGreen",
-        "RainbowViolet",
-        "RainbowCyan",
-      }
-
-      local hooks = require "ibl.hooks"
-      -- create the highlight groups in the highlight setup hook, so they are reset
-      -- every time the colorscheme changes
-      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-          vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-          vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-          vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-          vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-          vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-          vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-          vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-      end)
-
-      require("ibl").setup { indent = { highlight = highlight } }
-
+      require("ibl").setup {}
     end
   },
   'mattn/emmet-vim',
@@ -180,7 +155,7 @@ vim.opt.foldmethod="indent"
 vim.opt.foldnestmax=10
 vim.opt.wrap=true
 vim.opt.mouse="a" 
-vim.opt.foldlevel=2
+vim.opt.foldlevel=99
 vim.opt.laststatus=2
 vim.opt.splitbelow=true
 vim.opt.clipboard="unnamedplus"
